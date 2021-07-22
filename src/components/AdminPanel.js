ab64f0074363d2813f2f08b02b825f23586dbed7
cristianjav <cristian@example.com>
2021-07-21 23:40:14 -0300

    Agregado en Administración el campo para setear los minutos de espera y la dirección del contrato para stackear

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,24 +1,27 @@
-import { Contract } from "@ethersproject/contracts"
-import { useEffect, useState, useContext } from "react"
-import { useWeb3Context } from "web3-react"
-import ContractContext from "../utils/contractContext"
+import { Contract } from "@ethersproject/contracts";
+import { useEffect, useState, useContext } from "react";
+import { useWeb3Context } from "web3-react";
+import ContractContext from "../utils/contractContext";
 import { Row, Col, Card, Button, InputGroup, FormControl } from "react-bootstrap";
 
 function Admin() {
     const contractData = useContext(ContractContext);
     const context = useWeb3Context();
 
+    /// Declaro el contrato acá para hacerlo una sola vez
+    const signer = context.library.getSigner(context.account);
+    const contract = new Contract(contractData.address, contractData.abi, signer);
+
     const [isManager, setIsManager] = useState();
     const [amountTkn, setAmountTkn] = useState();
+    const [minutosTkn, setMinutosTkn] = useState();
+    const [stackerAddressTkn, setStackerAddressTkn] = useState();
 
     useEffect(() => {
         checkIfManager();
     });
 
     async function checkIfManager() {
-        const signer = context.library.getSigner(context.account);
-        const contract = new Contract(contractData.address, contractData.abi, signer);
-
         let manager = await contract.comprobarSiManager(context.account);
         if (manager) {
             console.log("Es: ", context.account);
@@ -29,9 +32,15 @@ function Admin() {
     }
 
     async function setAmount() {
-        const signer = context.library.getSigner(context.account);
-        const contract = new Contract(contractData.address, contractData.abi, signer);
         contract.setAmount(amountTkn);
+    }
+
+    async function setMinutos() {
+        contract.setMinutos(minutosTkn);
+    }
+
+    async function setStakerAddress() {
+        contract.setStackerAddress(stackerAddressTkn);
     }
 
     return (
@@ -42,7 +51,7 @@ function Admin() {
                     <Card>
                         <Card.Body>
                             <Card.Title>
-                                <p>Administración</p>
+                                <p>Cantidad de Tokens</p>
                             </Card.Title>
                             <InputGroup className='mb-3'>
                                 <InputGroup.Prepend>
@@ -50,14 +59,46 @@ function Admin() {
                                 </InputGroup.Prepend>
                                 <FormControl onChange={e => setAmountTkn(e.target.value)} placeholder='Cantidad que debe entregar la faucet.' />
                             </InputGroup>
-                            <Button onClick={setAmount}>Guardar Cambios</Button>
+                            <Button onClick={setAmount}>Guardar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>
+                                <p>Cantidad de Minutos</p>
+                            </Card.Title>
+                            <InputGroup className='mb-3'>
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text>Minutos:</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl onChange={e => setMinutosTkn(e.target.value)} placeholder='Cantidad que debe esperar el user.' />
+                            </InputGroup>
+                            <Button onClick={setMinutos}>Guardar</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>
+                                <p>Stacker Address</p>
+                            </Card.Title>
+                            <InputGroup className='mb-3'>
+                                <InputGroup.Prepend>
+                                    <InputGroup.Text>Address:</InputGroup.Text>
+                                </InputGroup.Prepend>
+                                <FormControl onChange={e => setStackerAddressTkn(e.target.value)} placeholder='Address del contrato que stakea.' />
+                            </InputGroup>
+                            <Button onClick={setStakerAddress}>Guardar</Button>
                         </Card.Body>
                     </Card>
                 </Col>
             </Row>
            : null }
         </div>
-    )
+    );
 }
 
-export default Admin
+export default Admin;
